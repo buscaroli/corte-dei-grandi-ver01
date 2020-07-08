cd853b79d7a70871274a865d96f0f1ddede99a6d
buscaroli <m.buscaroli@outlook.com>
2020-07-08 21:53:14 +0100

    Fixed minor issues with Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import '../styles/footer.css'
 
 const Footer = () => {
     return(
-        <div class="ui menu grid">
-            <p id="footer">Se vi piace <span id="cursive">La Corte dei Grandi</span> Condividete!  <i class="fab fa-facebook"></i>    <i class="fab fa-instagram"></i></p>
+        <div id="footer" className="ui menu">
+            <p id="text" className="ui">Se vi piace <span id="cursive">La Corte dei Grandi </span> condividete! <i class="fab fa-facebook"></i> <i class="fab fa-instagram"></i> <i class="fab fa-twitter"></i></p>
         </div>
     )
 }
